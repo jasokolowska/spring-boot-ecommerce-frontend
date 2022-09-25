a059c871c943572ebf16c2addd2a9d71ce78f335
Joanna Sokolowska <joanna_s@op.pl>
2022-09-25 10:59:23 +0200

    Login/Logout: setup app configuration for OpenID Connect

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa6n1t4x9T8MVq7Q5d7',
+        issuer: 'https://dev-26572567.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
